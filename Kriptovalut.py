9c6209bbf6ef4f25e104d53613d1e27b4a3771f6
Roma <rouom@mail.ru>
2024-08-11 16:55:31 +0300

    Создана функция exchange для получения из интернета курсов криптовалют

--- Kriptovalut.py
+++ Kriptovalut.py
@@ -4,6 +4,23 @@ from tkinter import *
 from tkinter import messagebox as mb
 
 
+def exchange():
+    code = entry.get()
+
+    if code:
+        try:
+            response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ripple,solana,tether,tron,cardano,dai,kaspa,pepe,litecoin&vs_currencies=usd')
+            response.raise_for_status()
+            data = response.json()
+            if code in data:
+                exchange_rate = data[code]
+                mb.showinfo("Курс обмена криптовалюты", f"Курс: {exchange_rate} {code} за 1 доллар")
+            else:
+                mb.showerror("Ошибка!", f"криптовалюта {code} не найдена")
+        except Exception as e:
+            mb.showerror("Ошибка",f"Произошла ошибка: {e}")
+    else:
+        mb.showwarning("Внимание!","Введите криптовалюту")
 
 
 window = Tk()
